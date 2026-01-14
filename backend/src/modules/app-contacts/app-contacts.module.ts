4486aabf963f220e9759042c00bd51a18bb7bbb2
Stu Wood <souporstu@gmail.com>
2026-01-14 15:07:31 -0800

    delete outdated

--- backend/src/modules/app-contacts/app-contacts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppContactsController } from './app-contacts.controller';
-import { AppContactsService } from './app-contacts.service';
-
-@Module({
-  controllers: [AppContactsController],
-  providers: [AppContactsService],
-})
-export class AppContactsModule {}
